2eb8d5b655f39a4c308adc905731e77a1e691ecb
VladislavMarychev <vladislav.marychev@yandex.ru>
2023-07-06 20:23:47 +0300

    Домашка готова

---
+++ Example4/Program.cs
@@ -0,0 +1,12 @@
+﻿//Напишите программу, которая на вход принимает число (N > 0), а на выходе показывает все чётные числа от 1 до N.
+
+Console.WriteLine("Введите число");
+int NumberN = Convert.ToInt32(Console.ReadLine()!);
+ for (int i = 1; i < NumberN; i++)
+ {
+    if (i % 2 == 0)
+    {
+        Console.Write(i + " ");
+        i++;
+    }
+ }
