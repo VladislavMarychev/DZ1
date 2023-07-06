0f5faf0562d1a5c0ba47872c12cc24e7b8f1da02
VladislavMarychev <vladislav.marychev@yandex.ru>
2023-07-06 19:10:44 +0300

    Первая домашка готова

---
+++ Example1/program.cs
@@ -0,0 +1,19 @@
+﻿//Напишите программу, которая на вход принимает два числа и выдаёт, какое число большее, а какое меньшее. 
+Console.WriteLine ("Введите число A");
+
+ int numberA = Convert.ToInt32(Console.ReadLine()!);
+
+ Console.WriteLine("Введите число B"); 
+
+ int numberB = Convert.ToInt32(Console.ReadLine()!);
+
+ if(numberA > numberB) 
+
+ { 
+    Console.WriteLine ("{0} Максимальное число", numberA ); 
+    Console.WriteLine ("{0} Минимальное число", numberB ); 
+    } 
+ else {
+     Console.WriteLine ("{0} Максимальное число", numberB );
+     Console.WriteLine ("{0} Минимальное число", numberA ); 
+     }
